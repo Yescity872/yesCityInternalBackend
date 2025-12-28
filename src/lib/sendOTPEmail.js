96a5bb6e50dc64f928dd33c935a8c46645e46a7a
YesCity872 <yescitycompany@gmail.com>
2025-12-28 06:31:30 +0530

    done auth new routes

--- src/lib/sendOTPEmail.js
+++ src/lib/sendOTPEmail.js
@@ -7,8 +7,8 @@ export async function sendOTPEmail(email, otp) {
     port: process.env.EMAIL_PORT || 587,
     secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
